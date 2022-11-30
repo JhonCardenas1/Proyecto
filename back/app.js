41c84c38d3061c3d4b093740bad183dedca3cb6c
JhonCardenas1 <jhon_7246@hotmail.com>
2022-11-30 12:46:17 -0500

    Configuracion back funciona

--- back/app.js
+++ back/app.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const app = express();
 
+//Importar las rutas
+const productos =require("./routes/products")
+
+app.use(express.json());
+
+app.use('/api', productos)
+
 
 module.exports = app
