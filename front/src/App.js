9b16f113ce823441d1de04085181581803ac006f
JhonCardenas1 <jhon_7246@hotmail.com>
2022-12-01 13:21:07 -0500

    Inicio del front adelanto

---
+++ front/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Tienda de tecnología informatica</h1>
+      <img src="./images/LogoTienda.jpg" alt="aqui va el logo"></img>
+      
+    </div>
+  );
+}
+
+export default App;
